06fce28f50101102bc1e1405811a2579b437bfd2
Tran Huy <huytran161201@gmail.com>
2022-06-11 17:09:16 +0700

    reup2

--- sleepApp/urls.py
+++ sleepApp/urls.py
@@ -3,6 +3,6 @@ from sleepApp import views
 
 urlpatterns = [
     path('',views.UserApi.as_view()),
-    path('login',views.AccountApi.as_view()),
+    path('login/username=<str:username>&password=<str:password>',views.AccountApi.as_view()),
     path('<str:Id>',views.UserApi.as_view()),
 ] 
